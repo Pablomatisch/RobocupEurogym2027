9a12ca0e6b24498216c2e0a7821a5054d1cffe34
pablomatisch <paulspitaler10@gmail.com>
2026-04-11 17:07:01 +0200

    updated code with new linefollower

--- SpikePrime_Code.py
+++ SpikePrime_Code.py
@@ -21,15 +21,15 @@ fd = port.C
 
 #values for calibrating color sensors
 CALIBRATION_MIN_VALID = 3
-black_reflection = 100
-white_reflection = 0
-reflection_treshold = (black_reflection + white_reflection)/2
+black_reflection = 60
+white_reflection = 100
+reflection_threshold = (black_reflection + white_reflection)/2
 
 #set speeds
 SPEED_STRAIGHT_FORWARD = 290
-SPEED_TURN_HIGH = SPEED_STRAIGHT_FORWARD - 40
-SPEED_TURN_LOW = SPEED_STRAIGHT_FORWARD + 20
-SPEED_SLOW = 160
+SPEED_TURN_HIGH = SPEED_STRAIGHT_FORWARD + 910
+SPEED_TURN_LOW = -SPEED_STRAIGHT_FORWARD + 385
+SPEED_SLOW = 70
 
 #important values
 TURN_180_TIME = 1.7/ 330 * SPEED_STRAIGHT_FORWARD
@@ -41,31 +41,40 @@ last_color_right = "white"
 last_color_left = "white"
 SILVER_TRESHOLD = 1017
 
+#store how long it was since last color seeings in case of hard turn
+since_last_black_right = 500
+since_last_black_left = 500
 #for zone handling
+
 ZONE_TRESHOLD = 220
 previous_distance = 10000
 
 #define the motor pair
 motor_pair.pair(motor_pair.PAIR_1, lm, rm)
 
-def color_is_black(port: int):
-    return color_sensor.reflection(port) < reflection_treshold
+def color_is_black(*ports: int):
+    return all(
+        color_sensor.reflection(port) < reflection_threshold and not color_is_green(port)
+        for port in ports
+    )
 
-def color_is_white(port: int):
-    return color_sensor.reflection(port) > reflection_treshold
+def color_is_white(*ports: int):
+    return all(
+        color_sensor.reflection(port) > reflection_threshold and not color_is_green(port)
+        for port in ports
+    )
 
-def color_is_green(port: int):
-    r, g, b, intensity = color_sensor.rgbi(port)
+def color_is_green(*ports: int):
+    for port in ports:
+        r, g, b, intensity = color_sensor.rgbi(port)
 
-    # avoid black / very dark readings
-    if intensity < 300:
-        return False
+        if intensity < 300:
+            return False
 
-    #check if green is greater than red
-    if g > r * 1.3 and g >= b * 1.02:
-        return True
+        if not (g > r * 1.3 and g >= b * 1.02):
+            return False
 
-    return False
+    return True
 
 def color_is_silver(port: int):
     r, g, b, intensity = color_sensor.rgbi(port)
@@ -161,8 +170,10 @@ async def drive_straight(distance_cm: float,
 
     while moved_degrees < target_degrees:
         if stop_at_black:
-            if color_is_black(fc):
+            if color_is_black(rc):
+                print("stopped at black")
                 motor_pair.stop(motor_pair.PAIR_1)
+                set_motors_straight_forward()
                 return True
         # SPIKE 3: tilt_angles()[0] gives yaw in deci-degrees with inverted sign
         yaw_deg = motion_sensor.tilt_angles()[0] * -0.1
@@ -176,7 +187,7 @@ async def drive_straight(distance_cm: float,
     motor_pair.stop(motor_pair.PAIR_1)
     return False
 
-async def rotate_degrees(rotate_degrees: float, velocity: int = SPEED_STRAIGHT_FORWARD):
+async def rotate_degrees(rotate_degrees: float, stop_at_black: bool = False, velocity: int = SPEED_STRAIGHT_FORWARD):
     """
     Turns the robot for a several degrees.
     Args:
@@ -190,6 +201,14 @@ async def rotate_degrees(rotate_degrees: float, velocity: int = SPEED_STRAIGHT_F
     rotated_degrees = 0
 
     while abs(rotate_degrees) > rotated_degrees:
+        if stop_at_black:
+            if color_is_black(rc):
+                motor_pair.stop(motor_pair.PAIR_1)
+                last_color_left = "white"
+                last_color_right = "right"
+                since_last_black_right += 1
+                set_motors_straight_forward()
+                return True
         motor_pair.move(motor_pair.PAIR_1, steer, velocity=velocity)
         rotated_degrees = abs(motion_sensor.tilt_angles()[0]*0.1)
         # calculate decellarateion relative to angle left to rotate
@@ -201,21 +220,29 @@ def update_last_colors():
     """ Checks and updates the last seen color of the left and right color sensors. """
     global last_color_right
     global last_color_left
+    global since_last_black_right
+    global since_last_black_left
     #save the last color the right sensor sees
-    if (color_is_black(rc) and (last_color_right != "black") and last_color_right != "green" and not color_is_green(rc)):
-        last_color_right = "black"
-    if (color_is_white(rc) and (last_color_right != "white") and not color_is_green(rc)):
+    since_last_black_right += 1
+    since_last_black_left += 1
+    if color_is_black(rc):
+        since_last_black_right = 0
+        if last_color_right != "black" and last_color_right != "green":
+            last_color_right = "black"
+    if color_is_white(rc) and last_color_right != "white":
         last_color_right = "white"
-    if (last_color_right != "green") and color_is_green(rc):
+    if last_color_right != "green" and color_is_green(rc):
         print("saved green")
         last_color_right = "green"
 
     #save the last color the left sensor sees
-    if (color_is_black(lc) and (last_color_left != "black") and last_color_left != "green" and not color_is_green(lc)):
-        last_color_left = "black"
-    if (color_is_white(lc) and (last_color_left != "white") and not color_is_green(lc)):
+    if color_is_black(lc):
+        since_last_black_left = 0
+        if last_color_left != "black" and last_color_left != "green":
+            last_color_left = "black"
+    if color_is_white(lc) and last_color_left != "white":
         last_color_left = "white"
-    if ((last_color_left != "green") and color_is_green(lc)):
+    if last_color_left != "green" and color_is_green(lc):
         print("saved green")
         last_color_left = "green"
 
@@ -228,7 +255,7 @@ def check_for_turns():
     global last_color_left
     global last_color_right
     #check colors on the ground in case there is a turn
-    if color_is_green(lc) and color_is_green(rc):
+    if color_is_green(rc, lc):
         print("did a full turn")
         light_matrix.show_image(light_matrix.IMAGE_ARROW_S)
         last_color_right = "green"
@@ -241,7 +268,7 @@ def check_for_turns():
         light_matrix.show_image(light_matrix.IMAGE_ARROW_N)
 
     #turn right if black follows to green
-    if color_is_black(rc) and last_color_right == "green" and not color_is_green(rc):
+    if color_is_black(rc) and last_color_right == "green":
             print("turned right")
             light_matrix.show_image(light_matrix.IMAGE_ARROW_E)
             set_motors_turn_right()
@@ -251,8 +278,8 @@ def check_for_turns():
             last_color_right = "green"
             light_matrix.show_image(light_matrix.IMAGE_ARROW_N)
         
-    #turn right if black follows to green
-    if color_is_black(lc) and last_color_left == "green" and not color_is_green(lc):
+    #turn left if black follows to green
+    if color_is_black(lc) and last_color_left == "green":
             print("turned left")
             light_matrix.show_image(light_matrix.IMAGE_ARROW_W)
             set_motors_turn_left()
@@ -287,14 +314,14 @@ async def check_for_zone():
     global ZONE_TRESHOLD
     global previous_distance
     """Checks if entered the zone and tries to escape it"""
-    if color_is_silver(fc) and distance_sensor.distance(fd) > 500:
+    if color_is_silver(rc) and distance_sensor.distance(fd) > 500:
         print("distance and color from zone:", distance_sensor.distance(fd))
         light_matrix.show_image(light_matrix.IMAGE_DIAMOND)
         await drive_straight(1)
         sleep(0.5)
         await drive_straight(1)
         if color_is_silver(rc):
-            if color_is_white(fc) and color_is_white(rc) and color_is_white(lc):
+            if color_is_white(rc, lc):
                 print("entered zone")
                 await drive_straight(10)
                 await rotate_degrees(10)
@@ -317,51 +344,49 @@ async def correct_line_path():
 
     Also handles the end of the course
     """
+    global since_last_black_left
+    global since_last_black_right
 
-    #drive straight forward wenn forward color is black
-    if (color_is_black(fc)) and color_is_white(rc) and color_is_white(lc):
-        set_motors_straight_forward()
-    else:
-        #turn left if left color is black
-        if (color_is_black(lc)):
-            set_motors_turn_left()
-           #turn right if right color is black
-        if (color_is_black(rc)):
-            set_motors_turn_right()
-        #drive forward to cross the goal line and the quit the program if forward color is red
-        if (color_sensor.color(fc) == color.RED):
-            await drive_straight(2)
+    #handles end of track
+    if color_sensor.color(fc) == color.RED:
+            await drive_straight(3)
             exit()
 
-def update_calibration():
-    """updates the current calibration values based on new data from sensors"""
-    global white_reflection
-    global black_reflection
-    global reflection_treshold
-    for sensor in [fc, rc, lc]:
-        #take multiple reading to smooth spikes
-        readings = [color_sensor.reflection(sensor) for _ in range(5)]
-        value = sum(readings) / len(readings)
+    #handles hard turns by checking which sensor saw the black line last in case both are white and then turns in that direction until black again
+    if color_is_white(fc, rc, lc) and since_last_black_right <= 200:
+        print(since_last_black_right)
+        stop_motors()
+        print("hard turn right")
+        await drive_straight(-1)
+        await rotate_degrees(-90, True)
+        set_motors_straight_forward()
+    #do the same with left
+    elif color_is_white(fc, rc, lc) and since_last_black_left <= 200:
+        print(since_last_black_left)
+        stop_motors()
+        print("hard turn left")
+        await drive_straight(-1)
+        await rotate_degrees(90, True)
+        set_motors_straight_forward()
 
-        #ignore invalid reading
-        if value < CALIBRATION_MIN_VALID:
-            continue
-        #update white if higher
-        if value > white_reflection:
-            white_reflection = value
-            #print("updated white reflection to: ", white_reflection)
-        #update black if lower
-        if value < black_reflection:
-            black_reflection = value
-            #print("updated black reflection to: ", black_reflection)
-    reflection_treshold = (white_reflection + black_reflection) / 2
+    #drive straight forward wenn forward color is black
+    if color_is_white(rc, lc):
+        set_motors_straight_forward()
 
+    elif color_is_black(rc, lc):
+        set_motors_straight_forward()
+
+    elif color_is_black(lc):
+        set_motors_turn_left()
+
+    elif color_is_black(rc):
+        set_motors_turn_right()
+        
 async def main():
     #Linefollower workcycle and main function
     light_matrix.show_image(light_matrix.IMAGE_ARROW_N)
     set_motors_straight_forward()
     while True:
-        update_calibration()
         update_last_colors()
         check_for_turns()
         await check_for_zone()
